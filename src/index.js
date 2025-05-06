a7adcd5bb373bc16e2019262de4ed535ceb22316
Chae Jisung <js.chae@alpacax.com>
2025-05-06 21:54:11 +0900

    Minor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // index.js
 import { HashRouter } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
